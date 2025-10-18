ee65cb0cbe1a773193e59b38eaade0edfe47e886
michaelkimm <alstjrdl951@gmail.com>
2025-10-18 22:33:32 +0900

    12-60~63: move on! 3D scroll 18~21

--- interactive-web/lecture/how-to-start-interative-web-development-properly/section12/js/character.js
+++ interactive-web/lecture/how-to-start-interative-web-development-properly/section12/js/character.js
@@ -1,33 +1,47 @@
-function Character({ xPos }) {
-  this.mainElem = document.createElement('div');
-  this.mainElem.classList.add('character');
-  this.mainElem.innerHTML = ''
-  + '<div class="character-face-con character-head">'
-      + '<div class="character-face character-head-face face-front"></div>'
-      + '<div class="character-face character-head-face face-back"></div>'
-  + '</div>'
-  + '<div class="character-face-con character-torso">'
-      + '<div class="character-face character-torso-face face-front"></div>'
-      + '<div class="character-face character-torso-face face-back"></div>'
-  + '</div>'
-  + '<div class="character-face-con character-arm character-arm-right">'
-      + '<div class="character-face character-arm-face face-front"></div>'
-      + '<div class="character-face character-arm-face face-back"></div>'
-  + '</div>'
-  + '<div class="character-face-con character-arm character-arm-left">'
-      + '<div class="character-face character-arm-face face-front"></div>'
-      + '<div class="character-face character-arm-face face-back"></div>'
-  + '</div>'
-  + '<div class="character-face-con character-leg character-leg-right">'
-      + '<div class="character-face character-leg-face face-front"></div>'
-      + '<div class="character-face character-leg-face face-back"></div>'
-  + '</div>'
-  + '<div class="character-face-con character-leg character-leg-left">'
-      + '<div class="character-face character-leg-face face-front"></div>'
-      + '<div class="character-face character-leg-face face-back"></div>'
-  + '</div>';
+class Character {
+    constructor({xPos}) {
+        this.mainElem = document.createElement('div');
+        this.mainElem.classList.add('character');
+        this.mainElem.innerHTML = ''
+        + '<div class="character-face-con character-head">'
+            + '<div class="character-face character-head-face face-front"></div>'
+            + '<div class="character-face character-head-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-torso">'
+            + '<div class="character-face character-torso-face face-front"></div>'
+            + '<div class="character-face character-torso-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-arm character-arm-right">'
+            + '<div class="character-face character-arm-face face-front"></div>'
+            + '<div class="character-face character-arm-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-arm character-arm-left">'
+            + '<div class="character-face character-arm-face face-front"></div>'
+            + '<div class="character-face character-arm-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-leg character-leg-right">'
+            + '<div class="character-face character-leg-face face-front"></div>'
+            + '<div class="character-face character-leg-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-leg character-leg-left">'
+            + '<div class="character-face character-leg-face face-front"></div>'
+            + '<div class="character-face character-leg-face face-back"></div>'
+        + '</div>';
 
-  document.querySelector('.stage').appendChild(this.mainElem);
+        document.querySelector('.stage').appendChild(this.mainElem);
 
-  this.mainElem.style.left = `${xPos}%`;
+        this.mainElem.style.left = `${xPos}%`;
+    }
+
+    changeDirection(direction) {
+        this.mainElem.setAttribute('data-direction', direction);
+    }
+
+    run() {
+        this.mainElem.classList.add('running');
+    }
+
+    stop() {
+        this.mainElem.classList.remove('running');
+    }
 }
--- interactive-web/lecture/how-to-start-interative-web-development-properly/section12/js/wall3d.js
+++ interactive-web/lecture/how-to-start-interative-web-development-properly/section12/js/wall3d.js
@@ -1,8 +1,10 @@
-    {
+{
     const stageEme = document.querySelector('.stage');
     const houseElem = document.querySelector('.house');
     const mousePos = { x: 0, y: 0 };
     let maxScrollValue;
+    let lastScrollY;
+    let characters = [];
 
     function resizeHandler() {
         maxScrollValue = document.body.offsetHeight - window.innerHeight;
@@ -12,11 +14,16 @@
     window.addEventListener('scroll', () => {
         const scrollRadio = window.scrollY / maxScrollValue;
 
-        const zMove = scrollRadio * 970 - 490;
-        houseElem.style.transform = `translateZ(${zMove}vw)`;
+        moveHouse(houseElem, scrollRadio);
+        updateCharacterDirection(characters, lastScrollY, window.scrollY);
+        updateCharacterAnimation(characters, true);
+        updateProgressBar(scrollRadio);
 
-        const progressElem = document.querySelector('.progress-bar');
-        progressElem.style.width = `${scrollRadio * 100}%`;
+        lastScrollY = window.scrollY;
+    });
+
+    window.addEventListener('scrollend', () => {
+        updateCharacterAnimation(characters, false);
     });
 
     window.addEventListener('mousemove', (e) => {
@@ -31,8 +38,47 @@
     resizeHandler();
 
     window.addEventListener('click', (e) => {
-        new Character({
+        const character = new Character({
             xPos: e.clientX / window.innerWidth * 100
         });
+        characters.push(character);
     });
+}
+
+function moveHouse(houseElem, scrollRadio) {
+    const zMove = scrollRadio * 970 - 490;
+    houseElem.style.transform = `translateZ(${zMove}vw)`;
+}
+
+function updateCharacterDirection(characters, lastScrollY, currentScrollY) {
+    if (lastScrollY === undefined) {
+        lastScrollY = currentScrollY;
+    }
+    const scrollDiff = currentScrollY - lastScrollY;
+
+
+    if (scrollDiff >= 0) {
+        for (const character of characters) {
+            character.changeDirection('forward');
+        }
+    } else {
+        for (const character of characters) {
+            character.changeDirection('backward');
+        }
+    }
+}
+
+function updateCharacterAnimation(characters, isRunning) {
+    for (const character of characters) {
+        if (isRunning) {
+            character.run();
+        } else {
+            character.stop();
+        }
+    }
+}
+
+function updateProgressBar(scrollRadio) {
+    const progressElem = document.querySelector('.progress-bar');
+    progressElem.style.width = `${scrollRadio * 100}%`;
 }
