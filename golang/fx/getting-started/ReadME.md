208f91aa3f0e2f89910c304749bebe6aff3a0a95
michaelkimm <alstjrdl951@gmail.com>
2025-10-23 09:36:47 +0900

    golang-fx-getting-started: Add an HTTP server

--- golang/fx/getting-started/ReadME.md
+++ golang/fx/getting-started/ReadME.md
@@ -1,8 +1,17 @@
-# About fx.New method
+# fx.New method
 New(..) method에 fx와 함께 실행할 컴포넌트를 등록할 수 있음.
 
-# About App.Run method
-앱을 실행 시키는 메소드. 앱이 stop signal을 받기 전까지 block된다. signal을 받은 이후엔 앱 자원을 정리하는 로직을 실행한다.
+# App.Run method
+앱을 실행 시키는 메소드. 앱이 stop signal을 받기 전까지 block됨. signal을 받은 이후엔 앱 자원을 정리하는 로직을 실행함.
+
+# fx.Provide method
+- 생성자들을 등록 시키는 함수. 애플리케이션에 인스턴스를 만드는 방법을 알려줌. 
+- 여러 타입의 객체를 리턴하는 생성자를 등록할 수도 있음. 생성자는 다수의 파라미터를 받을 수도 있음. 
+- Provide로 만들어진 인스턴스들은 기본적으로 캐싱되어 재사용될 수 있다. 싱글톤으로 활용됨. 
+- Provide 주입 순서는 중요하지 않음(의존성 그래프를 먼저 생성하지 않을까 싶음).
+
+### Reference
+https://github.com/uber-go/fx/blob/6fab1b2d3a549a67dfcf50b96161a887181c2afa/provide.go#L50
 
 # Reference
 https://uber-go.github.io/fx/get-started/minimal.html
--- golang/fx/getting-started/main.go
+++ golang/fx/getting-started/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "go.uber.org/fx"
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+
+	"go.uber.org/fx"
+)
 
 func main() {
-	fx.New().Run()
+	fx.New(
+		fx.Provide(NewHTTPServer),        // *http.Server 타입을 반환하는 NewHTTPServer provider 등록
+		fx.Invoke(func(*http.Server) {}), // *http.Server를 의존성 그래프에 등록시키는 역할
+	).Run()
+}
+
+func NewHTTPServer(lc fx.Lifecycle) *http.Server {
+	srv := &http.Server{Addr: ":8080"}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println("Starting HTTP server at", srv.Addr)
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+
+	return srv
 }
